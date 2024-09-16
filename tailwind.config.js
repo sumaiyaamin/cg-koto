fe5794a8d5c7af542a21a5eb6555de3fa4bfda8f
sumaiyaamin <sumaiya.prova321@gmail.com>
2024-09-17 02:44:12 +0600

    removed tailwind cdn

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./script.js",  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
